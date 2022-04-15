e6130aa6edc3ea4a8eb4b6d6a27084d779619c9f
Ethan Faggett <ethanf6@gmail.com>
2022-04-15 10:23:25 -0400

    adding image data

---
+++ Assignments/MoviesDatabase/MoviesDatabase/Models/MovieFav.swift
@@ -0,0 +1,8 @@
+//
+//  MovieFav.swift
+//  MoviesDatabase
+//
+//  Created by Ethan Faggett on 4/14/22.
+//
+
+import Foundation
---
+++ Assignments/MoviesDatabase/MoviesDatabase/Models/MovieResponseAux.swift
@@ -0,0 +1,8 @@
+//
+//  MovieResponseAux.swift
+//  MoviesDatabase
+//
+//  Created by Ethan Faggett on 4/14/22.
+//
+
+import Foundation
